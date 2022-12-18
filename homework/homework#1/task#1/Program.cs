544c3215cb13cca6b5b3ee354aa33bc4316671dc
LAV <5958383@mail.ru>
2022-12-18 22:56:29 +0300

    Зделал задачу №1

---
+++ homework/homework#1/task#1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Вариант с внесением числа вручную через консоль
+/*Console.WriteLine("Введи первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введи второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);*/
+
+// Вариант с получением двух случайных числел от 0 до 100
+
+int numberA = new Random().Next(0, 101);
+int numberB = new Random().Next(0, 101);
+
+
+if (numberA == numberB)
+{
+    Console.WriteLine($"Два числа одинаковых Первое число: {numberA} Второе число: {numberB}");
+}
+else if (numberA > numberB)
+{
+    Console.WriteLine($"Первое число больше второго: {numberA} Первое число: {numberA} Второе число: {numberB}");
+}
+else
+{
+   Console.WriteLine($"Второе число больше первого: {numberB} Первое число: {numberA} Второе число: {numberB}"); 
+}
