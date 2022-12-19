9eb5705a4e256299a09cf732ccf00fbe3e993b6c
LAV <5958383@mail.ru>
2022-12-19 11:45:06 +0300

    Сделал задачу №4

---
+++ homework/homework#1/task#4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вариант с внесением числа вручную через консоль
+/*Console.WriteLine("Введи любое число: ");
+int number = int.Parse(Console.ReadLine()!);*/
+
+int number = new Random().Next(1, 101);
+int i = 0;
+Console.Write($"число {number}");
+Console.WriteLine();
+while (number < i)
+{
+   if (i%2 == 0)
+   {
+    Console.WriteLine($"Являеться четным числом {i}");
+   }
+    
+   i++; 
+}
