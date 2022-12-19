2830cac66579db6cd348873c98294a07d13dfd6e
LAV <5958383@mail.ru>
2022-12-19 11:18:57 +0300

    Выполнил задачу №3

---
+++ homework/homework#1/task#3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вариант с внесением числа вручную через консоль
+/*Console.WriteLine("Введи любое число: ");
+int number = int.Parse(Console.ReadLine()!);*/
+
+int number = new Random().Next(0, 101);
+
+if (number%2 == 0 )
+{
+   Console.WriteLine($"Число четное: {number}");
+}
+else
+{
+    Console.WriteLine($"Число нечетное: {number}");
+}
