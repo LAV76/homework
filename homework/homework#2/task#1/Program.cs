3dc64efa4d72897105f0e71a3b0e315b7e3469de
LAV <5958383@mail.ru>
2022-12-19 20:49:38 +0300

    Сделал задачу №1

---
+++ homework/homework#2/task#1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вариант с внесением числа вручную через консоль
+Console.WriteLine("Введи любое трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+//int number = new Random().Next(100, 1000);
+
+
+if ((99<number) && (number<1000))
+{
+    Console.Write($"Трехзначное число: {number} Второй знак числа: {((number%100)-(number%100)%10)/10}");
+}
+else
+{
+    Console.Write($"Число вне заданого диапазона");
+}
