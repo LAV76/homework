833d5940bb2ec7dd7331a11b2ba4fe90f0a5b2ba
LAV <5958383@mail.ru>
2022-12-25 00:36:14 +0300

    Сделал задачу №2

--- homework/homework#3/task#2/Program.cs
+++ homework/homework#3/task#2/Program.cs
@@ -12,4 +12,4 @@ System.Console.Write("Введите коордианту y точки B: ");
 int z2 = Convert.ToInt32(Console.ReadLine());
 
 double result = Math.Round(Math.Sqrt(Math.Pow((x1 - x2),2) + Math.Pow((y1-y2),2)+ Math.Pow((z1-z2),2)),2); 
-System.Console.WriteLine($"Расстояние между двумя точками по трем осям координат: {result}");
+System.Console.WriteLine($"Расстояние между двумя точками по трем осям координат: {result}"); 
