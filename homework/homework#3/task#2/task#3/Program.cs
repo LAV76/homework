d80e0d36ae7e4154b24cea8016c7d3570b7e9cc2
LAV <5958383@mail.ru>
2022-12-25 00:32:51 +0300

    Сделал задачу №3

---
+++ homework/homework#3/task#2/task#3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вариант с внесением числа вручную через консоль
+Console.WriteLine("Введи любое натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+
+//int number = Convert.ToInt32(new Random().Next(1, 20));
+
+Console.Write($"таблицу кубов чисел от 1 до {number} ");
+
+if (number>0)
+{
+   for (int i = 1; i <= number; i++)
+{
+   Console.Write(Math.Pow(i, 3)+" ");
+} 
+}
+else
+{
+    Console.Write($"Число: {number} не натуральное");
+}
+
