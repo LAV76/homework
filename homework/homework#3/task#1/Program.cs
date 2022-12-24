0e9f7848e7cc3db3897ed680416312c6ece7486c
LAV <5958383@mail.ru>
2022-12-24 23:53:30 +0300

    Сделал задачу №1 через разложение

--- homework/homework#3/task#1/Program.cs
+++ homework/homework#3/task#1/Program.cs
@@ -1,15 +1,21 @@
 ﻿// Вариант с внесением числа вручную через консоль
-// Console.WriteLine("Введи любое число: ");
-//string number = Console.ReadLine()!;
+// Console.WriteLine("Введи любое 5-ти значное число: ");
+//int number = Convert.ToInt32(Console.ReadLine()!);
 
-string number = Convert.ToString (new Random().Next(10000, 100001));
+ int number = Convert.ToInt32(new Random().Next(10000, 100001));
 
-if (number == Convert.ToString(number[4])+Convert.ToString(number[3])+Convert.ToString(number[2])+Convert.ToString(number[1])+Convert.ToString(number[0]))
+ int number5 =(number-(number%10000))/10000;
+ int number4 = ((number%10000)/1000)*10;
+ int number3 = ((number%1000)/100)*100;
+ int number2 = ((number%100)/10)*1000;
+ int number1 = (number%10)*10000;
+
+if (number==(number5+number4+number3+number2+number1))
 {
-    Console.Write ($" Палиндромом: {number} Состоит из: {number[0]} {number[1]} {number[2]} {number[3]} {number[4]}"); 
+    Console.Write ($"Палиндромом: {number} {number5+number4+number3+number2+number1}"); 
 }
 else 
 {
-    Console.Write ($" Число {number} не являеться палиндромом");
+    Console.Write ($"Число {number} не являеться палиндромом");
 }
 
