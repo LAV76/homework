f8cdd9882952fdc0caad8a899b594e2dfc1cccfb
LAV <5958383@mail.ru>
2022-12-24 22:51:41 +0300

    Сделал задачу №1 через массив

--- homework/homework#3/task#1/Program.cs
+++ homework/homework#3/task#1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Вариант с внесением числа вручную через консоль
+// Console.WriteLine("Введи любое число: ");
+//string number = Console.ReadLine()!;
+
+string number = Convert.ToString (new Random().Next(10000, 100001));
+
+if (number == Convert.ToString(number[4])+Convert.ToString(number[3])+Convert.ToString(number[2])+Convert.ToString(number[1])+Convert.ToString(number[0]))
+{
+    Console.Write ($" Палиндромом: {number} Состоит из: {number[0]} {number[1]} {number[2]} {number[3]} {number[4]}"); 
+}
+else 
+{
+    Console.Write ($" Число {number} не являеться палиндромом");
+}
+
