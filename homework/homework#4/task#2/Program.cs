fae343d237f4ed678f1d0d34b531ea392d857d86
LAV <5958383@mail.ru>
2023-01-13 20:10:46 +0300

    Сделал задачу №2 математическа ветка math_new

--- homework/homework#4/task#2/Program.cs
+++ homework/homework#4/task#2/Program.cs
@@ -1,9 +1,7 @@
 ﻿Console.WriteLine ("Ввкдите целое число: ");
 string number = Console.ReadLine()!;
 
-
 Console.WriteLine ($"Сумма всех чисел числа {number} равна {SumNumber(number)} ");
-
 
 int SumNumber(string number){
  int count = 0;
