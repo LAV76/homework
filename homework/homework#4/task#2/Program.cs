79052890ef4da966d944449cb5031b504b0ca900
LAV <5958383@mail.ru>
2023-01-13 18:09:49 +0300

    Сделал задачу №2 через строку

--- homework/homework#4/task#2/Program.cs
+++ homework/homework#4/task#2/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.WriteLine ("Ввкдите целое число: ");
+string number = Console.ReadLine()!;
+
+
+Console.WriteLine ($"Сумма всех чисел числа {number} равна {SumNumber(number)} ");
+
+
+int SumNumber(string number){
+ int count = 0;
+    for(int i = 0; i < number.Length; i++){
+        count=count+Convert.ToInt32(Convert.ToString(number[i]));
+        Console.WriteLine(Convert.ToInt32(Convert.ToString(number[i])));
+    }
+return count;
+}
+
