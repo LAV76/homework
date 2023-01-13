057c740558ca4c42c2d2768fa45ab5692c935115
LAV <5958383@mail.ru>
2023-01-13 19:34:51 +0300

    Сделал задачу №3

---
+++ homework/homework#4/task#3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] Array = GetArray(8); 
+Console.Write($"[{String.Join(";", Array)}]"); 
+ 
+int[] GetArray (int size) 
+{ 
+    int[] Array = new int[size]; 
+    for (int i = 0; i < size; i++) 
+    { 
+        Array[i] = new Random().Next(0, 100); 
+    } 
+    return Array; 
+}
