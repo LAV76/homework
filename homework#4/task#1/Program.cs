059851f7fac234638df7048d28cbb078adb45f08
LAV <5958383@mail.ru>
2023-01-13 17:28:50 +0300

    Сделал задачу №1

---
+++ homework#4/task#1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Ввкдите число: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine ("Ввкдите степень: ");
+int extent = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine ($"Возведение числа {number} в степень {extent} равно {SqrCount (number, extent)} ");
+
+
+int SqrCount(int number, int extent){
+ int count = 1;
+    for(int i = 0; i < extent; i++){
+        count=count*number;
+    }
+return count;
+}
