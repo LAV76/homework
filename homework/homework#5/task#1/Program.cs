3c17efec38be3c4c66a1b0100883f24c6f9c261c
LAV <5958383@mail.ru>
2023-01-18 22:39:02 +0300

    Сделал задачу №1

---
+++ homework/homework#5/task#1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int[] array = GetArray(8);
+Console.WriteLine($"В массие: [{String.Join(", ", array)}] {GerCount(array)} четных чисел");
+
+//----------------Методы-----
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+       for(int i = 0; i < size; i++)
+       {
+            res[i] = new Random().Next(100, 1000);
+       }
+    return res;
+}
+
+int GerCount(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if ((array[i]%2) == 0) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
