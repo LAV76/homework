c63e1cab5b6e72683e74ba859fc4991122b7d511
LAV <5958383@mail.ru>
2023-01-19 09:18:53 +0300

    Сделал задачу №2

---
+++ homework/homework#5/task#2/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] array = GetArray(5);
+Console.WriteLine($"В массие: [{String.Join(", ", array)}] сумма стоящих на нечетных элементах массива{GerCount(array)}");
+
+//----------------Методы-----
+
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+       for(int i = 0; i < size; i++)
+       {
+            res[i] = new Random().Next(-10, 10);
+       }
+    return res;
+}
+
+int GerCount(int[] array)
+{
+    int count = 0;
+    for(int i = 1; i < array.Length; i+=2)
+    {
+        count = count + array[i];
+    }
+    return count;
+}
