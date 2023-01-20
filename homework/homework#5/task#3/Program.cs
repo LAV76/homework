3727d2364f335e7eb78c32a675d7c749196bc689
LAV <5958383@mail.ru>
2023-01-20 11:53:04 +0300

    Исправил  задачу №3

--- homework/homework#5/task#3/Program.cs
+++ homework/homework#5/task#3/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] array = GetArray(6);
+﻿int[] array = GetArray(5);
 Console.WriteLine($"В массие: [{String.Join(", ", array)}] произведение пар [{String.Join(", ",GerCount(array))}]");
 
 //----------------Методы-----
@@ -15,12 +15,15 @@ int[] GetArray(int size)
 
 int[] GerCount(int[] array)
 {
-    int size = array.Length/2;
+  int size = array.Length/2;
+  if (array.Length%2 == 1) size++;
+
     int[] res = new int[size];
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < array.Length/2; i++)
     {
       res[i] =  array[i]*array[(array.Length-1)-i];
-    }
+    } 
+    if (array.Length%2 == 1) res[^1] = array[array.Length/2];
     return res;
 }
 
