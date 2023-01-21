a3ffc04a5d5e70312affaea9ca7d2eb2958e10a7
LAV <5958383@mail.ru>
2023-01-21 23:03:31 +0300

    Сделал задачу №1 с помощью стороки

---
+++ homework/homework#6/task#1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите произвольное количество чисел, через пробел после окончания набора нажмите Enter");
+String StrinArr = Console.ReadLine()!;
+int i = 0;
+//ConsoleKeyInfo kay;
+//do
+//{
+    //kay = Console.ReadKey();
+    string[] num = StrinArr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    //int number = Convert.ToInt32(Console.ReadLine()!);
+    //if (number>0) i++;
+
+    
+//} while (kay.Key != ConsoleKey.Escape);
+foreach (var item in num)
+{
+    if (item != "0") i++; 
+}
+Console.WriteLine($"Количество введеных чисел [{String.Join("|", num)}] больше 0 = {i}");
