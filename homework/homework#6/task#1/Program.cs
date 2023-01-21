4e0fb760af2b84d866f37904ee08c608dc0f882c
LAV <5958383@mail.ru>
2023-01-21 23:17:45 +0300

    Сделал задачу №1 с помощью отслеживания клавиш

--- homework/homework#6/task#1/Program.cs
+++ homework/homework#6/task#1/Program.cs
@@ -1,18 +1,13 @@
 ﻿Console.WriteLine("Введите произвольное количество чисел, через пробел после окончания набора нажмите Enter");
-String StrinArr = Console.ReadLine()!;
 int i = 0;
-//ConsoleKeyInfo kay;
-//do
-//{
-    //kay = Console.ReadKey();
-    string[] num = StrinArr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    //int number = Convert.ToInt32(Console.ReadLine()!);
-    //if (number>0) i++;
+ConsoleKeyInfo kay;
+do
+{
+    kay = Console.ReadKey();
+    int number = Convert.ToInt32(Console.ReadLine()!);
+    if (number>0) i++;
 
     
-//} while (kay.Key != ConsoleKey.Escape);
-foreach (var item in num)
-{
-    if (item != "0") i++; 
-}
-Console.WriteLine($"Количество введеных чисел [{String.Join("|", num)}] больше 0 = {i}");
+} while (kay.Key != ConsoleKey.Escape);
+
+Console.WriteLine($"Количество введеных чисел больше 0 = {i}");
