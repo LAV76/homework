01e00d5a799695bd0083b7d21949f9730d4ac662
LAV <5958383@mail.ru>
2023-01-21 23:19:50 +0300

    Сделал задачу №1 с помощью стороки

--- homework/homework#6/task#1/Program.cs
+++ homework/homework#6/task#1/Program.cs
@@ -1,16 +1,7 @@
 ﻿Console.WriteLine("Введите произвольное количество чисел, через пробел после окончания набора нажмите Enter");
 String StrinArr = Console.ReadLine()!;
 int i = 0;
-//ConsoleKeyInfo kay;
-//do
-//{
-    //kay = Console.ReadKey();
-    string[] num = StrinArr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    //int number = Convert.ToInt32(Console.ReadLine()!);
-    //if (number>0) i++;
-
-    
-//} while (kay.Key != ConsoleKey.Escape);
+string[] num = StrinArr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 foreach (var item in num)
 {
     if (item != "0") i++; 
