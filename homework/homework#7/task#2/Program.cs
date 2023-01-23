c7ea61118e2e6d2f71c379958481cd5ef38e38b5
LAV <5958383@mail.ru>
2023-01-23 17:09:20 +0300

    Сделал задачу №2

---
+++ homework/homework#7/task#2/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int rows =new Random().Next(1, 9);
+int columns =new Random().Next(1, 9);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+if (SearchNumber(array, number)) {Console.Write($"Введенное число {number} есть масиве");}
+else {Console.Write($"Введенное число {number} отсутствует в масиве");}
+
+
+
+
+// -------------------Методы---------------------------------
+int[,] GetArray(int m, int n, int minValue, int maxValue){
+    int[,] result = new int[m,n];
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            result[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool SearchNumber(int[,] array, int number){
+    bool result = false;
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            if(array[i, j] == number ) result = true;
+        }
+    }
+    return result;
+}
