e3cdc7fb42b20174a980edb821d15b366947787e
LAV <5958383@mail.ru>
2023-01-23 16:21:17 +0300

    Сделал задачу №1

---
+++ homework/homework#7/task#1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+
+
+// -------------------Методы---------------------------------
+double[,] GetArray(int m, int n){
+    double[,] result = new double[m,n];
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            result[i,j] = Math.Round((new Random().Next(-10, 11)) + (new Random().NextDouble()), 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
