ca4b61e9dded388ec9c9afa51f5bfb26f916f6ae
LAV <5958383@mail.ru>
2023-01-27 12:55:26 +0300

    Сделал задачу №1

---
+++ homework/homework#8/task#1/Program.cs
@@ -0,0 +1,58 @@
+﻿int rows =new Random().Next(2, 9);
+int columns =new Random().Next(2, 9);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+for(int i = 0; i < array.GetLength(0); i++)
+{
+	SortArray(array,  i);
+}
+Console.WriteLine();
+PrintArray(array);
+
+
+
+// -------------------Методы---------------------------------
+int[,] GetArray(int m, int n, int minValue, int maxValue){
+    int[,] result = new int[m,n];
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            result[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+	{
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int [,] array, int row)
+{
+    bool swapped;
+
+    do
+    {
+        swapped = false;
+        for (int i = 1; i < array.GetLength(1); i++) {
+            if (array[row, i - 1] < array[row, i])
+            {
+                
+				int temp = array[row, i - 1];
+				array[row, i - 1] = array[row, i];
+				array[row, i] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped != false);
+}
+ 
+
