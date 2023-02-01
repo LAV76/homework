0518a2c27f28e7ffa3ae6405969c17b1bee51dce
LAV <5958383@mail.ru>
2023-02-01 22:18:56 +0300

    Сделал задачу №3

---
+++ homework/homework#9/task#3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введи первое число промежутка : ");
+nint N  = nint.Parse(Console.ReadLine()!);
+Console.Write("Введи первое число промежутка : ");
+nint M  = nint.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Ackerman(N, M)); 
+
+// ------------------Метод------------------
+
+
+nint Ackerman(nint N, nint M)
+{
+  if (N == 0)
+    return M + 1;
+  else
+    if ((N > 0) && (M == 0))
+      return Ackerman(N - 1, 1);
+    else
+      return Ackerman(N - 1, Ackerman(N, M - 1));
+}
