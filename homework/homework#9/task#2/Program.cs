0685f3455673f9f4f25c99013dc485b7f5c924bc
LAV <5958383@mail.ru>
2023-02-01 21:49:11 +0300

    Сделал задачу №2

---
+++ homework/homework#9/task#2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи первое число промежутка : ");
+int N  = int.Parse(Console.ReadLine()!);
+Console.Write("Введи первое число промежутка : ");
+int M  = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N, M)); 
+
+// ------------------Метод------------------
+int PrintNumbers(int N, int M){
+    if(M == 0) return (N * (N + 1)) / 2;
+    if(M ==N) return M;
+    if(M < N) return N + PrintNumbers(N-1, M);
+    return N + PrintNumbers(N+1, M);
+}
