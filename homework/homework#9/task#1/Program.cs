840c2e7987854bfd9f49d81d9ebb30b87c672c34
LAV <5958383@mail.ru>
2023-02-01 21:26:40 +0300

    Сделал задачу №1

---
+++ homework/homework#9/task#1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введи число: ");
+int N  = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N)); 
+
+// ------------------Метод------------------
+string PrintNumbers(int N){
+    if(N < 1) return ""; 
+    return (N + " " + PrintNumbers(N-1));
+}
